ff89306582688b2b0658b7d6b299dc8854d4e660
Fabricio Junior <fabricio.zanotelli.jr@gmail.com>
2021-05-31 11:44:40 +0200

    Iclusao de um algoritmo para entrega de caixas para um problema da OBI

---
+++ caixa-drone.py
@@ -0,0 +1,29 @@
+caixaaa = 0
+caixabe = 0
+caixace = 0
+
+while caixaaa < 1 or caixaaa > caixabe or caixabe > caixace or caixace >1000:
+    caixaaa = int(input('O tamanho da caixa A é de:'))
+    caixabe = int(input('O tamanho da caixa B é de:'))
+    caixace = int(input('O tamanho da caixa C é de:'))
+
+
+if caixaaa < caixabe:
+    if caixace == caixabe:
+        print('Será necessário apenas duas viagens!')
+    elif caixace > caixabe:
+        print('Será necessário apenas uma viagem!')
+elif caixaaa == caixabe:
+    if caixaaa + caixabe < caixace:
+        print('Será necessário apenas uma viagens!')
+    else:
+        print('Serão necessárias três viagens!')
+elif caixabe < caixace:
+    if caixaaa < caixabe:
+        print('Será necessário apenas uma viagem!')
+    else:
+        if caixaaa + caixabe < caixace:
+            print('Será necessario apenas uma viagem!')
+        else:
+            print('Será necessário apenas duas viagens!')
+print('.')
