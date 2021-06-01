712f7c766da187b587d61691f3a9729268a78757
Fabricio Junior <fabricio.zanotelli.jr@gmail.com>
2021-06-01 11:43:01 +0200

    Inclusao do algoritmo para resoluçao do problema domino da OBI, para descobrir a quantidade de peças encontradas no jogo

---
+++ OBI-2019/Fase 1/domino-OBI.py
@@ -0,0 +1,7 @@
+numnat = -1
+while numnat < 0 or numnat > 100:
+    numnat = int(input('O maior numero de circulos em um dos lados da peça do seu jogo é: '))
+
+numpec = ((numnat + 1) * (numnat + 2))/2
+
+print('Esse jogo tem ' + numpec.__str__() + ' peças')
